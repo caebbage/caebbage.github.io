17e8fc50f0d9dd8eb34c01c6ed19dadea51c5056
cae <iyphir@gmail.com>
2018-02-05 13:10:38 -0500

    180205: new design

---
+++ assets/script/dark.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function () {
+  if (Cookies.get('dark')) {
+      $('body').addClass('dark');
+  }
+
+  $('#lights').on('click', function () {
+    if ($('body').hasClass('dark')) {
+      $('body').removeClass();
+      Cookies.set('dark', 0, {expires: 90})
+    } else {
+      $('body').addClass('dark');
+      Cookies.set('dark', 1, {expires: 90})
+    }
+  });
+});
